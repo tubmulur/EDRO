13f0ba070a12dd75136218440a7e1b1a34f725b2
Andrey Chekmarev <assminog@gmail.com>
2020-06-16 04:46:25 +0300

    Update logics

--- 1.Event/Event.php
+++ 1.Event/Event.php
@@ -1,149 +1,40 @@
 <?php
 /*© A.A.CheckMaRev assminog@gmail.com tubmulur@yandex.ru*/
-class Event
+class Event extends Design
 	{
-	public function __construct($_objKIIM, $obj)
+	public function __construct($_objKIIM)
 		{
 		$objKIIM=$_objKIIM;unset($_objKIIM);
 		$objKIIM=KIIM::objStart($objKIIM, array('_strClass'=>__CLASS__, '_strMethod'=>__FUNCTION__, '_strMessage'=>''));
 
-		//$this->strBasePath	=Reality::strBasePath();
-		$strRequest=strGetRequest();
-		if(bIzEvent('/robots.txt', $strRequest))
+		$strEvent						=strGetEventSetter();
+		$arrEventParams						=arrGetEventSetterParams($strEvent);
+		if(is_array($this->arrEDRO[$strEvent]))
 			{
-			header('Content-Type: text/plain');
-			echo Read::str($objKIIM,'robots.txt');
-			exit(0);
-			}
-		elseif(bIzEvent('/favicon.ico', $strRequest))
-			{
-			header('Content-Type: image/png');
-			echo Read::str($objKIIM,'favicon.jpg');
-			exit(0);
-			}
-		elseif(bIzEvent('/', $strRequest))
-			{
-			$this->strEvent		=
-				array(
-					'strName'	=>'',
-					'strAction'	=>'/',
-					'arrParams'	=>arrGetRequest($strEvent, $strRequest)
-					);
-			$this->strDesign	=
-				array(
-					'strBasePath'	=>'/home/EDRO',
-					'FileList'	=>
-						array(
-							'strTitle'	=>'HiFiIntelligentClub',
-							'arrElement'	=>
-								array(
-									//..
-									),
-							),
-
-					);
-			$this->arrReality	=
-				array(
-					'strBasePath'	=>'/home/EDRO',
-					'arrRoles'	=>
-						array(
-							'Listener'=>
-								array(
-									'strObject'=>'FileList',
-									'strDesign'=>'FileList',
-									),
-							'Dj'	  =>
-								array(
-									'strObject'=>'FileList',
-									'strDesign'=>'FileList',
-									),
-							),
-					);
-			$this->arrObject	=
-				array(	'FileList'=>
-					array(
-						'strClassName'	=>'FileList',
-						'strAction'	=>'strGetHTML',
-						'strTarget'	=>'/home/EDRO.PolymerConstructor/Main',
-						),
-					'DjFileList'=>
-					array(
-						'strClassName'	=>'FileList',
-						'strAction'	=>'strGetHTML',
-						'strTarget'	=>'/home/EDRO.PolymerConstructor/Main',
-						),
-					);
-			$this->strAction	='/home/EDRO.PolymerConstructor/Main';
-			$this->strMethod	='strHTML';
-			}
-		elseif(bIzEvent('/stationList', $strRequest))
-			{
-			$this->strEvent		='/stationList';
-			$this->strAction	='stationList';
-			$this->arrParams	=arrGetRequest($strEvent, $strRequest);
-			}
-		elseif(bIzEvent('/fileList', $strRequest))
-			{
-			$this->strEvent		='/fileList';
-			$this->strAction	='fileList';
-			$this->arrParams	=arrGetRequest($strEvent, $strRequest);
-			}
-		elseif(bIzEvent('/station/search', $strRequest))
-			{
-			$this->strEvent		='/station/search';
-			$this->strAction	='station/search';
-			$this->arrParams	=arrGetRequest($strEvent, $strRequest);
-
-			}
-		elseif(bIzEvent('/Master', $strRequest))
-			{
-			$this->strEvent		='/Master';
-			$this->strAction	='Master';
-			$this->arrParams	=arrGetRequest($strEvent, $strRequest);
-			}
-		elseif(bIzEvent('/Follow', $strRequest))
-			{
-			$this->strEvent		='/Follow';
-			$this->strAction	='Follow';
-			$this->arrParams	=arrGetRequest($strEvent, $strRequest);
-			}
-		elseif(bIzEvent('/News', $strRequest))
-			{
-			$this->strEvent		='/News';
-			$this->strAction	='News';
-			$this->arrParams	=arrGetRequest($strEvent, $strRequest);
-			}
-		elseif(bIzEvent('/Podcast', $strRequest))
-			{
-			$this->strEvent		='/Podcast';
-			$this->strAction	='Podcast';
-			$this->arrParams	=arrGetRequest($strEvent, $strRequest);
-			}
-		elseif(bIzEvent('/BentleyMusic_Квартирный_Вопрос', $strRequest))
-			{
-			$this->strEvent		='/BentleyMusic_Квартирный_Вопрос';
-			$this->strAction	='BentleyMusic_Квартирный_Вопрос';
-			$this->arrParams	=arrGetRequest($strEvent, $strRequest);
+			$this->arrEDRO						=$this->arrEDRO[$strEvent];
+			$this->arrEDRO['arrEvent']['strAction']			=$strEvent;
+			$this->arrEDRO['arrEvent']['arrParams']			=arrPrepareEventParams($this->arrEDRO['arrEvent']['arrParams'], $arrEventParams);
+			$this->arrEDRO['arrEvent']['bIzDynamic']		=$this->bIsDynamic($objKIIM);
 			}
 		else
 			{
+			$objError=new ReportError($objKIIM, $strEvent.' is out of published events.');
 			exit(0);
 			}
-
-		$this->bIsDynamic=$this->bIsDynamic($objKIIM);
-		/*echo'<pre>';
-			print_r($this);
-		echo'</pre>';*/
+		parent::__construct($objKIIM);
 
 		KIIM::objFinish($objKIIM, array('_strClass'=>__CLASS__, '_strMethod'=>__FUNCTION__, '_strMessage'=>''));
 		}
+
 	private function bIsDynamic($_objKIIM)
-		{$objKIIM=$_objKIIM;unset($_objKIIM);$objKIIM=KIIM::objStart($objKIIM, array('_strClass'=>__CLASS__, '_strMethod'=>__FUNCTION__, '_strMessage'=>''));
+		{
+		$objKIIM=$_objKIIM;unset($_objKIIM);
+		$objKIIM=KIIM::objStart($objKIIM, array('_strClass'=>__CLASS__, '_strMethod'=>__FUNCTION__, '_strMessage'=>''));
 
 		$bIsDynamic=false;
-		if(isset($this->objEvent->arrRequest[0]))
+		if(isset($this->objEvent->arrRequest))
 			{
-			if($this->objEvent->arrRequest[0]=='GetServiceDj')
+			if($this->objEvent->arrRequest=='GetServiceDj')
 				{
 				$bIsDynamic=true;
 				}
@@ -156,7 +47,6 @@ class Event
 		KIIM::objFinish($objKIIM, array('_strClass'=>__CLASS__, '_strMethod'=>__FUNCTION__, '_strMessage'=>''));
 		return $bIsDynamic;
 		}
-
 	public static function strInitJs()
 		{
 		//Event.Design.Reality
@@ -206,6 +96,15 @@ class Event
 						}
 					}
 				}
+		</script>
+		<?php
+		}
+	public static function strDeclareOsciloscope()
+		{
+		?>
+		<script>
+			objOscilloscope		=document.getElementById('oscilloscope');
+			_Flow1			=setInterval(_FlowSync, 1000);
 		</script>
 		<?php
 		}

3d1f69e6575e409c61b48d678b1bcee2afcf9f2f
Andrey Chekmarev <assminog@gmail.com>
2020-06-23 01:44:40 +0300

    Небольшие правки.

--- 2.Design/Design.php
+++ 2.Design/Design.php
@@ -30,6 +30,26 @@ class Design extends Reality
 			constructor()
 				{
 				console.log('Design constructor started');
+				//this.intScreenHeight	=screen.height
+				this.intScreenHeight	=objDynaScreen.intHeight;
+				this.intScreenWidth	=objDynaScreen.intWidth;
+				this.intHorElements	=Math.floor(this.intScreenWidth/419);
+				this.intVertElements	=Math.floor(this.intScreenHeight/90);
+				this.blockWidth		=Math.floor(this.intScreenWidth/this.intHorElements);
+				this.intTotalElements	=(this.intHorElements*this.intVertElements)-(this.intVertElements*2)-1;
+				
+				if(this.intTotalElements<=0)
+					{
+					this.intTotalElements=2;
+					}
+				objSearch.intOnPage	=this.intTotalElements;
+				objSearch.intBlockWidth	='';
+				objSearch.strEvent	='';
+				objSearch._CreateUrl();
+				objDynaScreen.strURL		=objSearch.strURL;
+				objEvent.strURL			=objSearch.strURL;
+				objDynaScreen._Update();
+
 				}
 			}
 		</script>

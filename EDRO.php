7d22f4494606b9f83b20ef67980c55ae7b223b21
Andrey Chekmarev <assminog@gmail.com>
2020-08-27 19:16:24 +0300

    70%

--- EDRO.php
+++ EDRO.php
@@ -1,133 +1,80 @@
 <?php
-/*© A.A.CheckMaRev assminog@gmail.com tubmulur@yandex.ru*/
-			    //////  /\ RCe:
-			       // <  **>
-			     //     Jl
-			   ////// 2020
+                     /*_____
+© Andrey Chekmaryov 2020
+
+Email:    assminog@gmail.com
+Email:    tubmulur@yandex.ru
+Phone:    +7(911)787-44-57
+Whatsapp: +7(911)787-44-57
+Telegram: https://t.me/HficSamin
+VK:       https://vk.com/Hfic.Samin
+VK:       https://vk.com/HiFiIntelligentClub
+Facebook: https://facebook.com/Hfic.Samin
+Facebook: https://facebook.com/HiFiIntelligentClub
+Site[Ru] Public browsing international:  http://HiFiIntelligentClub.Ru
+Site[En] Public browsing international:  http://HiFiIntelligentClub.COM
+Site[En] Private browsing international: http://ryklzxobxv4s32omimbu7d7t3cdw6dplvsz36zsqqu7ad2foo5m3tmad.onion
+|E    |D     |R      |O      |
+|Event|Design|Reality|Objects|
+ ////// 2020                   /////  / 
+//        /\                  // /   
+//      <  **>               /// /  
+ //////   jl                ///// /    
+./././././././*/
 class EDRO extends Event
 	{
 	protected $strBasePath;
 	public function __construct($_objKIIM)
-		{
-		$objKIIM	=$_objKIIM;unset($_objKIIM);
-		$objKIIM	=KIIM::objStart($objKIIM, array('_strClass'=>__CLASS__, '_strMethod'=>__FUNCTION__, '_strMessage'=>''));
-		$this->strBasePath		=Reality::strBasePath();
-		//obJSignal_To_Setter=   Human->. ('FileList','Language=EN');
+		{$objKIIM=$_objKIIM;unset($_objKIIM);$objKIIM=KIIM::objStart($objKIIM, array('_strClass'=>__CLASS__, '_strMethod'=>__FUNCTION__, '_strMessage'=>''));
 
-		$this->arrEDRO['/']['arrEvent']=
-			array(
-				// 'strAction'	=>'/',
-				'arrParams'=>array(
-					'name'		=>'',
-					'style'		=>'',
-					'bitrate'	=>'',
-					'codec'		=>'',
-					'page'		=>0,
-					),
-			);
-		$this->arrEDRO['/']['arrDesign']=
-			array(
-				'strTemplate'	=>'/home/EDRO/2.Design/.strFileList.php', //+
-				'strPos'	=>'Display',
-			);
-		$this->arrEDRO['/']['arrReality']=
-			array(
-				'strLang'=>'EN'
-			);
-		$this->arrEDRO['/']['arrObjects']=
-			array(
-				'arrSearchBlock'=>array(
-					'arrEvent'	=>array(),
-					'arrDesign'	=>array(),
-					'arrReality'	=>array(),
-					'arrObjects'	=>array(
-						'arrNameInput'=>array(
-							'arrEvent'	=>array(),
-							'arrDesign'	=>array(),
-							'arrReality'	=>array(
-								'strDbStart'	=>'/home/EDRO.Полимер/Name',
-								),
-							'arrObjects'	=>array(),
-							),
-						'arrGenreInput'=>array(
-							'arrEvent'	=>array(),
-							'arrDesign'	=>array(),
-							'arrReality'	=>array(
-								'strDbStart'	=>'/home/EDRO.Полимер/Genre',
-								),
-							'arrObjects'	=>array(),
-							),
-						'arrCodecInput'=>array(
-							'arrEvent'	=>array(),
-							'arrDesign'	=>array(),
-							'arrReality'	=>array(
-								'strDbStart'	=>'/home/EDRO.Полимер/Codec',
-								),
-							'arrObjects'	=>array(),
-							),
-						'arrBitrateInput'=>array(
-							'arrEvent'	=>array(),
-							'arrDesign'	=>array(),
-							'arrReality'	=>array(
-								'strDbStart'	=>'/home/EDRO.Полимер/Bitrate',
-								),
-							'arrObjects'	=>array(),
-							),
-						),
-					),
-				'arrDynaScreen'=>
-					array(
-					'arrEvent'	=>array(),
-					'arrDesign'	=>array(),
-					'arrReality'	=>array(),
-					'arrObjects'	=>array(
-						'arrFileList'=>array(
-							'arrEvent'	=>array(),
-							'arrDesign'	=>array(),
-							'arrReality'	=>array(
-								'strDbStart'	=>'/home/EDRO.Полимер/Stations/Name/unordered/',
-								),
-							'arrObjects'	=>array(
-								'arrStation'=>array(),
-							),
-						),
-					),
-				),
-			);
+		$this->strBasePath		=Reality::strBasePath();
 
 		parent::__construct($objKIIM);
-		//echo'<pre>';
-		//	print_r($this);
-		//echo'</pre>';
 		/*
-
-		*/
-		//$this->objEventGetter	=new Event($_objKIIM	,$arrSet);			//HFic++
-		//$this->objDesignGetter	=new Design($_objKIIM	,$this, $strObjectName);	//HFic++
-		//$this->objRealityGetter	=new Reality($_objKIIM	,$this);			//HFic++
-		//$this->objObjectsGetter	=new Objects($_objKIIM	,$this);			//HFic++
-
-
-		
-		Read::strGetDesignHTML($objKIIM, $this->arrEDRO['arrDesign']['strTemplate'], $this->arrEDRO);
-		/*
-		echo '<objDebug
-			class="block fixed layer_5_Debug scrollerY"
-			style="
-				top		:20px;
-				left		:0;
-				height		:80vh;
-				width		:100vw;
-				background-color:#FFF;
-				"
-			>
-			<pre>';
+		echo '<pre>';
 			print_r($this);
-		echo ' </pre>
-		     </objDebug>
-			';*/
-		//print_r($this);
+		echo '</pre>';
+		exit(0);
+		*/
+		FileRead::_GetDesignHTML($objKIIM, $this->arrDesign['strTemplate'], $this);
 		KIIM::objFinish($objKIIM, array('_strClass'=>__CLASS__, '_strMethod'=>__FUNCTION__, '_strMessage'=>''));
 		}
+
+	public static function strObjectDeclare()
+		{
+		$str	= <<<oo2oo
+		<script>
+			console.log('[V]EDRO: Declare');
+			class EDRO
+				{
+				constructor()
+					{
+					console.log('[Vv]EDRO: Construct');
+
+					//this.K			=0;
+					//this.L			=0;
+					this.intStep		=0;//Player session operation time
+					this.intVector		=0;
+					//this.strCurOperID;
+					
+
+					console.log('[..]EDRO: Construct');
+					}
+				}
+			console.log('[.]EDRO: Declare');
+		</script>
+oo2oo;
+		return $str;
+		}
+	public static function strObjectInit()
+		{
+		$str	= <<<oo2oo
+		<script>
+			console.log('[V]EDRO: Init');
+			objEDRO		=new EDRO();
+			console.log('[.]EDRO: Init');
+		</script>
+oo2oo;
+		return $str;
+		}
 	}
-	//	U->E-> D-> R->O

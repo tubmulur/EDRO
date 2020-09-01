f76875f1163100b19eb0acc04e17679d5b278c68
Andrey Chekmarev <assminog@gmail.com>
2020-09-01 18:48:27 +0300

    Add files via upload

--- 4.Objects/Write/Listener/Display/List/StationList.php
+++ 4.Objects/Write/Listener/Display/List/StationList.php
@@ -50,49 +50,49 @@ class StationList
 			'strHTML'=>''
 			),
 		);
-	public function __construct($_objKIIM, $_objEDRO)
-		{$objKIIM=$_objKIIM;unset($_objKIIM);$objKIIM=KIIM::objStart($objKIIM, array('_strClass'=>__CLASS__,'_strMethod'=>__FUNCTION__, '_strMessage'=>''));
-
-		$objEDRO	=$_objEDRO;
-			   unset($_objEDRO);
-		/*echo '<pre>';
-		print_r($objEDRO);
-		echo '</pre>';*/
-		/*echo '<pre>';
-		print_r($objEDRO->arrEvent['arrParams']);
-		echo '</pre>';*/
-		//!!echo'<script>'.$objEDRO->arrEvent['strObjectParams'].'</script>'; !!
-		//!!echo'<script>objEvent._UpdateURLDyn()</script>'; !!
-		//echo '$int0Page';
-		/*echo*/ $int0Page	=($objEDRO->arrEvent['arrParams']['int0Page']); //0,1,xxx
-		//echo '<br/>';
-		//echo '$int1OnPage';
-		/*echo*/ $int1OnPage	=$objEDRO->arrEvent['arrParams']['int1OnPage']; //1-> 8 = 8
-		//echo '<br/>';
-		//echo '$int0Start';
-		/*echo*/$int0Start	=0+($int0Page*$int1OnPage);//From 0 to 7 intStart=8 ->15 intStart=16;
-		//echo '<br/>';
-		//echo '$int1Untill';
-		/*echo*/ $int1Untill	=($int0Start+$int1OnPage);//From 0 to 7 including 7 = 8
-		//echo '<br/>';
-		$objTotal	=FileRead::objJSON($objKIIM, $objEDRO->arrObjects['сРасположениеTotal']); //0-lastone
-		//echo '$int1Total';
-		/*echo*/ $int1Total	=($objTotal->total); //Channge in par
-		//echo '<br/>';
-		unset($objTotal);
-		//echo '$int1Pages';
-		/*echo*/ $int1Pages	=intRoundUp(($int1Total)/$int1OnPage);//totall is not from 0, to find we need to convert ;
-		//echo '<br/>';
-		if($int0Page>$int1Pages)
+	public function __construct($_objKIIM, $objEDRO)
+		{
+		$arrPagination		=$this->arrPagination($_objKIIM, $objEDRO);
+		//print_r($arrPagination);
+		$_SESSION['arrPagination']=$arrPagination;
+		//print_r($_SESSION['arrPagination']);
+		//exit(0);
+		
+		for($int0I=$arrPagination['int0Start'];$int0I<=$arrPagination['int0Untill'];$int0I++)
 			{
-			$objEDRO->arrEvent['arrParams']['int0Page']	=($int1Pages-1);
-			$int0Page					=($int1Pages-1);
-			//echo '$int0Start';
-			/*echo*/ $int0Start	=0+($int0Page*$int1OnPage);//From 0 to 7 intStart=8 ->15 intStart=16;
-			//echo '<br/>';
-			//echo '$int1Untill';
+			//echo $int0I;
+			//echo'123';
+			$objStation	=FileRead::objJSON($objKIIM, $objEDRO->arrObjects['сРасположение'].'/'.$int0I.'.plmr');
+			$arrStatrion=
+			array(
+				'strName'		=>$objStation->server_name,
+				'strAudio'		=>сКодировать($objStation->listen_url, $_сДействие='к', $_сКлючь="HiFiIntelligentClub"),
+				'strAudioType'		=>$objStation->server_type,
+				'strAudioBitrate'	=>$objStation->bitrate,
+				'strStyle'		=>$objStation->genre,
+				);
+			$arrPagination['int0CurrentStation']=$int0I;
+
+			$this->strHTML.= StationBlock::strHTML($objKIIM, $arrStatrion, $arrPagination, $objEDRO->arrEvent['arrParams']);
+			}
+		}
+	private function arrPagination($_objKIIM, $objEDRO)
+		{
+		/*echo*/$int0Page	=($objEDRO->arrEvent['arrParams']['int0Page']); //0,1,xxx
+		/*echo*/$int1OnPage	=$objEDRO->arrEvent['arrParams']['int1OnPage']; //1-> 8 = 8
+		/*echo*/$int0Start	=0+($int0Page*$int1OnPage);//From 0 to 7 intStart=8 ->15 intStart=16;
+		/*echo*/$int1Untill	=($int0Start+$int1OnPage);//From 0 to 7 including 7 = 8
+		/*echo*/$objTotal	=FileRead::objJSON($objKIIM, $objEDRO->arrObjects['сРасположениеTotal']); //0-lastone
+		/*echo*/$int1Total	=($objTotal->total); //Channge in par
+		unset($objTotal);
+		/*echo*/$int1Pages	=intRoundUp(($int1Total)/$int1OnPage);//totall is not from 0, to find we need to convert ;
+		$int0Pages		=($int1Pages-1);
+		if($int0Page>$int0Pages)
+			{
+			$objEDRO->arrEvent['arrParams']['int0Page']	=$int0Pages;
+			$int0Page					=$int0Pages;
+			/*echo*/ $int0Start	=0+($int0Pages*$int1OnPage);//From 0 to 7 intStart=8 ->15 intStart=16;
 			/*echo*/ $int1Untill	=($int0Start+$int1OnPage);//From 0 to 7 including 7 = 8
-			//echo '<br/>';
 			}
 		else
 			{
@@ -108,82 +108,16 @@ class StationList
 			}
 		if($int1Total<$int1Start)
 			{
-			$int1Start=($int1Total-$int1OnPage);
+			$int0Start=($int1OnPage*$int0Page);
 			}
-		/*echo'<script>'.$objEDRO->arrEvent['strObjectParams'].'</script>';
-		echo'<pre>';
-		print_r($objEDRO);
-		echo'</pre>';
-		exit;*/
+		$arrReturn['int0Start']		=$int0Start;
+		$arrReturn['int0Page']		=$int0Page;
+		$arrReturn['int0Pages']		=$int0Pages;
+		$arrReturn['int1OnPage']	=$int1OnPage;
+		$arrReturn['int0Untill']	=$int0Untill;
+		$arrReturn['int0Total']		=($int1Total-1);
 
-
-
-
-
-		//$intMaxElements		=$intOnPage;
-
-
-		
-
-		
-		
-		
-
-		//echo'<script>'.$objEDRO->arrEvent['strObjectParams'].'</script>';
-		/*print_r($objEDRO->arrEvent['strObjectParams']);
-		exit;*/
-		//echo $objEDRO->arrObjects['сРасположение'];
-		//echo $int0_Start;
-		//echo '<br/>';
-		//echo $int0_Untill;
-		//echo '<br/>';
-		/*echo '$int0Start';
-		echo $int0Start;
-		echo '<br/>';
-		echo '$int0Untill';
-		echo $int0Untill;
-		echo '<br/>';*/
-		for($int0I=$int0Start;$int0I<=$int0Untill;$int0I++)
-			{
-			//echo $int0I.'<br/>';
-			$objStation	=FileRead::objJSON($objKIIM, $objEDRO->arrObjects['сРасположение'].'/'.$int0I.'.plmr');
-			$arrStatrion=
-			array(
-				'strName'		=>$objStation->server_name,
-				'strAudio'		=>сКодировать($objStation->listen_url, $_сДействие='к', $_сКлючь="HiFiIntelligentClub"),
-				'strAudioType'		=>$objStation->server_type,
-				'strAudioBitrate'	=>$objStation->bitrate,
-				'strStyle'		=>$objStation->genre,
-				);
-			/*echo '<pre>';
-			print_r($arrStatrion);
-			echo '</pre>';*/
-			$arrPagination=
-			array(
-				
-				'int0Page'		=>$int0Page,
-				'int1Pages'		=>$int1Pages,
-				'int1OnPage'		=>$int1OnPage,
-				'int0Total'		=>($int1Total-1),
-				'int0CurrentStation'	=>$int0I,
-				);
-			/*echo '<pre>';
-			print_r($arrPagination);
-			echo '</pre>';*/
-			/*echo '<pre>';
-			print_r($objEDRO);
-			echo '</pre>';*/
-			//$objEDRO;
-			//$this->strHTML.= StationBlock::strHTML($objKIIM, $arrStatrion, $arrPagination, $objEDRO);
-			/*
-			if($intTotal==$intI)
-				{
-				break;
-				}*/
-			}
-		//echo $this->strHTML;
-		echo HiFiNavigation::strHTML($objKIIM, $arrPagination, $objEDRO->arrEvent['arrParams']);
-		KIIM::objFinish($objKIIM, array('_strClass'=>__CLASS__, '_strMethod'=>__FUNCTION__, '_strMessage'=>''));
+		return $arrReturn;
 		}
 	public function _HTML($_objKIIM, $_objEDRO)
 		{
@@ -193,6 +127,11 @@ class StationList
 		{
 		$objStationList=new StationList($_objKIIM, $_objEDRO, $arrSearch);
 		return $objStationList->strHTML;
+		}
+	public function _EDRO($_objKIIM, $_objEDRO)
+		{
+		$objStationList=new StationList($_objKIIM, $_objEDRO, $arrSearch);
+		return $objStationList->strEDRO;
 		}
 	}
 ?>
